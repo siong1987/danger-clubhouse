ba0a7bc5084012a825f082dd2f38bfe29c946cc0
Simon Thiboutôt <simonthiboutot@hotmail.com>
2019-06-26 10:15:43 -0400

    Update YARD documentation.

--- lib/clubhouse/plugin.rb
+++ lib/clubhouse/plugin.rb
@@ -1,21 +1,21 @@
-module Danger
-  # This is your plugin class. Any attributes or methods you expose here will
-  # be available from within your Dangerfile.
-  #
-  # To be published on the Danger plugins site, you will need to have
-  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
-  # for generating documentation from your plugin source, and you can verify
-  # by running `danger plugins lint` or `bundle exec rake spec`.
-  #
-  # You should replace these comments with a public description of your library.
-  #
-  # @example Ensure people are well warned about merging on Mondays
-  #
-  #          my_plugin.warn_on_mondays
-  #
-  # @see  Teng Siong Ong/danger-clubhouse
-  # @tags monday, weekends, time, rattata
-  #
+# This is your plugin class. Any attributes or methods you expose here will
+# be available from within your Dangerfile.
+#
+# To be published on the Danger plugins site, you will need to have
+# the public interface documented. Danger uses [YARD](http://yardoc.org/)
+# for generating documentation from your plugin source, and you can verify
+# by running `danger plugins lint` or `bundle exec rake spec`.
+#
+# You should replace these comments with a public description of your library.
+#
+# @example Ensure people are well warned about merging on Mondays
+#
+#          my_plugin.warn_on_mondays
+#
+# @see  Teng Siong Ong/danger-clubhouse
+# @tags monday, weekends, time, rattata
+#
+module Danger  
   # This plugin will detect stories from clubhouse and link to them.
   #
   # @example Customize the clubhouse organization and check for stories to link to them.
